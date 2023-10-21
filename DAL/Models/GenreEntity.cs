165d1095d7e1a1fe5364689a111baeefcf5c662c
ivan_ceskic <ivan_ceskic@EPHRZAGL0004>
2023-10-21 18:21:37 +0200

    a

--- DAL/Models/GenreEntity.cs
+++ DAL/Models/GenreEntity.cs
@@ -6,29 +6,19 @@ namespace GameStore_DAL.Models
     public class GenreEntity : BaseEntity
     {
 
-        //[Required]
-        //public string GenreName { get; set; }
-        //
-        //public ICollection<GenreEntity>? SubGenre { get; set; }
-        //
-        //public int? ParentGenreId { get; set; }
-        //
-        //
-        //public ICollection<GameGenre>? GameGenres { get; set; }
-
         
 
             [Required]
         public string GenreName { get; set; }
 
-        // Add a navigation property to the parent genre
+        
         
 
         public int? ParentGenreId { get; set; }
 
         public GenreEntity? ParentGenre { get; set; }
 
-        // Add a navigation property to the collection of child genres
+        
         public ICollection<GenreEntity>? SubGenre { get; set; }
 
         public ICollection<GameGenre>? GameGenres { get; set; }
